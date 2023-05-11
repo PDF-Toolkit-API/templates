58c3e713456b123c4788f98f25255b0b088d7632
Jeremy <ferrer.jeremy3@gmail.com>
2023-05-11 11:05:58 +0200

    Finish 00-demo example

--- tutorials/00-demo/run-alt.sh
+++ tutorials/00-demo/run-alt.sh
@@ -1,5 +1,5 @@
-curl -X POST "http://127.0.0.1:8000/api/v1/convert/htmltopdf/" \
-  -H "Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE2ODMwOTg2MzguNTkzNjkyLCJuYmYiOjE2ODMwOTg2MzguNTkzNjkyLCJzdWIiOiJhcGlfa2V5OjU3YzVhY2IzLThmODEtNDI2MC04MGVmLWM2MTVlZTA3YmNiMyJ9.DND9wa4E2Df7Gy0RIS9XP6EgqNMpVIYPOirdLVSZcs0" \
+curl -X POST "https://pdftoolkitapi.com/api/v1/convert/htmltopdf/" \
+  -H "Authorization: Bearer YOUR_API_KEY" \
   -H "Content-Type: application/json" \
   --data '{"source": "'"$(cat tutorials/00-capabilities/file.html | tr '\n' ' ' | sed 's/\"/\\\"/g')"'",
   "header_template": "'"$(cat tutorials/00-capabilities/header.html | tr '\n' ' ' | sed 's/\"/\\\"/g')"'",
